8aab49e9fc61007d9ab3a77783d7e01b34f13878
Alex Bedritskiy <33065801+1albedr@users.noreply.github.com>
2017-11-22 06:11:57 +0200

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 const size_t SIZE = 9;
-void bubbleSort(int*, size_t);
-void initArray(int*, size_t);
-void printArray(int*, size_t);
-
-//int main()
-//{
-//	int nineNumbers[SIZE];
+void bubbleSort(int[], size_t);
+void initArray(int[], size_t);
+void printArray(int[], size_t);
+int main()
+{
+	int nineNumbers[SIZE];
 //	initArray(nineNumbers, SIZE);
 //	bubbleSort(nineNumbers, SIZE);
 //	printArray(nineNumbers, SIZE);
@@ -18,7 +17,7 @@ void printArray(int*, size_t);
 //	return 0;
 //}
 //
-//void bubbleSort(int* a, size_t n)
+//void bubbleSort(int a[], size_t n)
 //{
 //	int buffer{};
 //	for (int i = n - 1; i > 0; --i)
@@ -32,13 +31,14 @@ void printArray(int*, size_t);
 //			}
 //		}
 //}
-//void initArray(int* a , size_t n)
+//void initArray(int a[], size_t n)
 //{
 //	cout << "Enter " << n << " integer numbers: ";
 //	for (size_t i = 0; i < n; ++i)
 //		cin >> a[i];
 //}
-//void printArray(int* a, size_t n)
+//void printArray(int a[], size_t n)
+  
 //{
 //	for (size_t i = 0; i < n; ++i)
 //		cout << a[i] << " ";
